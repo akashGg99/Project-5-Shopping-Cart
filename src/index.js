c75251ef69beaf14adb18dfb3d6c3b43868b1a15
akashGg99 <aakkash15r@gmail.com>
2023-04-24 23:30:27 +0530

    minor changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-const express= require('express')
-const mongoose=require('mongoose')
-const route=require('./route/route')
-const multer=require('multer')
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const route = require('./route/route')
+const multer = require('multer')
 
-const app=express()
+
 app.use(express.json())
 app.use(multer().any())
+
+
 mongoose.set('strictQuery', true)
-mongoose.connect('mongodb+srv://sarwjeet424:96568437528p@cluster0.8tsocgw.mongodb.net/group31Database',{useNewUrlParser:true})
-.then(()=> console.log("MongoDB Connected"))
-.catch((err)=> console.log(err))
-let PORT = 3000 
-app.listen(PORT, ()=>{
-    console.log(`Connected on Port ${PORT}`)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.log(err))
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Connected on Port ${process.env.PORT}`)
+
 })
-app.use('/',route)
+app.use('/', route)
+
